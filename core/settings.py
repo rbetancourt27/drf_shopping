5cf1678b1f3ec5a0dbb25863c31710ab73e0aeab
Roberto Betancourt <rbetancourt@kavehome.com>
2023-01-31 19:03:41 +0100

    mod: change the way to get env vars

--- core/settings.py
+++ core/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("secret")
+SECRET_KEY = os.environ["secret"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG")
+DEBUG = os.environ["DEBUG"]
 
 ALLOWED_HOSTS = []
 
